e3726a46bc9029db91fe0d83d5e6ec776667ce17
Eduardo Barreira <edubarreira1212@gmail.com>
2023-08-30 03:43:59 -0300

    get and post method created

--- index.js
+++ index.js
@@ -7,3 +7,21 @@ app.listen(3000, () => {
     console.log("The server's ok!");
 })
 
+const cars = [];
+
+app.get("/cars", (req, res) =>{
+    console.log(req.method);
+    res.status(200).send({cars: cars});
+})
+
+app.get("/cars/:id", (req, res) =>{
+    console.log(req.method);
+    const car = cars.find(car => car.id == req.params.id);
+    res.status(200).send(car);
+})
+
+app.post("/cars" , (req, res) =>{
+    console.log(req.method);
+    cars.push(req.body);
+    res.status(200).send(req.body);
+})
