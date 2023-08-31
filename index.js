ad2e2d76047cafaff286976842a4ccd40560f1da
Eduardo Barreira <edubarreira1212@gmail.com>
2023-08-30 23:11:09 -0300

    put and delete method created

--- index.js
+++ index.js
@@ -9,19 +9,35 @@ app.listen(3000, () => {
 
 const cars = [];
 
-app.get("/cars", (req, res) =>{
+app.get("/cars", (req, res) => {
     console.log(req.method);
     res.status(200).send({cars: cars});
 })
 
-app.get("/cars/:id", (req, res) =>{
+app.get("/cars/:id", (req, res) => {
     console.log(req.method);
     const car = cars.find(car => car.id == req.params.id);
     res.status(200).send(car);
 })
 
-app.post("/cars" , (req, res) =>{
+app.post("/cars" , (req, res) => {
     console.log(req.method);
     cars.push(req.body);
     res.status(200).send(req.body);
+})
+
+app.put("/cars/:id", (req, res) => {
+    console.log(req.method);
+    const carIndex = cars.findIndex(car => car.id == req.params.id);
+    cars[carIndex] = req.body;
+    res.status(200).send(req.body);
+})
+
+app.delete("/cars/:id", (req, res) => {
+    console.log(req.method);
+    const carIndex = cars.findIndex(car => car.id == req.params.id);
+    if (carIndex !== -1) {
+        cars.splice(carIndex, 1);
+        res.status(200).send(cars[carIndex]);
+    }
 })
